e20bc37ec0c6c90d3ed2d41c81e31d795d646a1f
Johnny Le <johnnyle1410@gmail.com>
2025-09-23 20:39:21 +0200

    jedis testing in main

--- backend/src/main/java/com/example/demo/PollApplication.java
+++ backend/src/main/java/com/example/demo/PollApplication.java
@@ -4,11 +4,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.UnifiedJedis;
 
 @SpringBootApplication
 @RestController
 public class PollApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PollApplication.class, args);
+
+		// Redis Set experiment
+		UnifiedJedis jedis = new UnifiedJedis("redis://localhost:6379");
+		jedis.del("users");
+		jedis.sadd("users", "alice");
+		jedis.sadd("users", "bob");
+		jedis.srem("users", "alice");
+		jedis.sadd("users", "eve");
+		System.out.println("Currently logged in users: " + jedis.smembers("users"));
+		jedis.close();
 	}
 }
